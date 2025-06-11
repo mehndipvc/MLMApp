67219263ab453e0537c46bcb2aa5a67ad0f07cd5
mehndipvc <mehndipvc@gmail.com>
2025-06-11 20:30:02 +0530

    Update product.php

--- product.php
+++ product.php
@@ -9,10 +9,13 @@ if(!isset($_COOKIE['mobile']))
 <?php
 $cat_id=$_GET['id'];
 $sel_products = $obj->fetch("
-    SELECT * FROM items 
-    WHERE cat_id='$cat_id' 
+    SELECT * FROM (
+        SELECT * FROM items 
+        WHERE cat_id='$cat_id'
+    ) AS sorted_items 
     ORDER BY CAST(SUBSTRING(name, 3) AS UNSIGNED) ASC
 ");
+
 ?>
 <style>
 .card {
